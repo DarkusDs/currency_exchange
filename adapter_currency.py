eb7ff0d4308873e9c42ac31c98b0a4b43561186f
Dmytro Stelmakh <rex.st.04@gmail.com>
2025-12-03 17:07:00 +0200

    1. added realisation of docker-compose 2. added connection to MySQL

--- adapter_currency.py
+++ adapter_currency.py
@@ -1,27 +1,19 @@
 from datetime import datetime
 from typing import List, Optional
-from logger_setup import logger
 
 
-def unify_currency(bank: str):
+def unify_currency(mapper: dict):
     def unificator(func):
         def wrapper(date: datetime, valcode: Optional[str]) -> List[dict]:
             data = func(date, valcode)
             unified = []
-            for i in data:
-                if bank == "nbu":
-                    code = i.get("cc")
-                    name = i.get("txt") or code
-                    rate = i.get("rate")
-                elif bank == "privat":
-                    code = i.get("currency")
-                    name = i.get("currency") or code
-                    rate = i.get("saleRate")
-                else:
-                    logger.warning(f"Невідомий банк: {bank}")
-                    continue
 
-                unified.append({"code": code, "name": name, "rate": rate})
+            for i in data:
+                r = dict()
+                for k, v in mapper.items():
+                    r[k] = i.get(v)
+                unified.append(r)
+
             return unified
         return wrapper
     return unificator
--- api/privat.py
+++ api/privat.py
@@ -5,27 +5,16 @@ import requests
 from requests import URLRequired
 from logger_setup import logger
 from adapter_currency import unify_currency
+from .sender import _send_get_request
 
 
 @unify_currency('privat')
 def get_privat_exchange_rates(date: str, valcode: str = None) -> Optional[List[Dict]]:
-    res = []
-    try:
-        formated_date = datetime.strptime(date, '%Y%m%d').strftime('%d.%m.%Y')
-        url = 'https://api.privatbank.ua/p24api/exchange_rates'
-        params = {
-            'date': formated_date,
-        }
-        res = requests.get(url, params=params)
-        res = res.json().get("exchangeRate", [])
-        logger.info(f"Успішно отримано курс валюти {valcode} на дату {date}")
-    except HTTPError as e:
-        logger.error("HTTP Error")
-    except ConnectionError as e:
-        logger.error("ConnectionError")
-    except URLRequired as e:
-        logger.error("URLRequired")
-    except Exception as e:
-        logger.error(e)
-    finally:
-        return res
+    formated_date = datetime.strptime(date, '%Y%m%d').strftime('%d.%m.%Y')
+    url = 'https://api.privatbank.ua/p24api/exchange_rates'
+    params = {
+        'date': formated_date,
+    }
+    res = _send_get_request(url=url, params=params).get("exchangeRate", [])
+    logger.info(f"Успішно отримано курс валюти {valcode} на дату {date}")
+    return res
